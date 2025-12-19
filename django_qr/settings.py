b67b762b6d2c90793456a96131fecb6962856c3c
niddana mohan <you@example.com>
2025-12-20 00:41:29 +0530

    Add STATIC_ROOT for collectstatic

--- django_qr/settings.py
+++ django_qr/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #
 MEDIA_ROOT='/home/render/project/media'
 MEDIA_URL='/media/'
